5e9e895f8f9199b35fc3c0ec5b021c478e1dd8cc
pextech <mcstain1639@gmail.com>
2020-12-27 13:25:31 +0200

    ft-postgres-configuration -setted up database using postgres(elephantSQL as db) -used sequelize to track ORM

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-unused-vars */
 import express from 'express';
+import Sequelize from 'sequelize';
 import documentation from './documentation';
 
 const router = express.Router();
@@ -7,7 +9,6 @@ router.use('/', documentation);
 router.get('/', (req, res) => {
   res.status(200).json({ status: 200, message: 'Hey, you made it to my Duties generator app, have fun' });
 });
-
 router.use((req, res) => {
   const error = new Error('Page Not found');
   res.status(404).json({
