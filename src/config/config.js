524775e5e780f27006903b232ef608fd6f620a85
pextech <mcstain1639@gmail.com>
2021-01-03 12:24:08 +0200

    added babel core in dependencies

--- src/config/config.js
+++ src/config/config.js
@@ -17,10 +17,6 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'postgres',
+    use_env_variable: 'DATABASE_URL',
   },
 };
